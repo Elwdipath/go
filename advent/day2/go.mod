62836f1f425048e6263f971d5bde878c53a2d454
Jordan Kaiser <jordan.kaiser@gmail.com>
2026-06-11 11:28:30 -0400

    day 2 part 2 complete

--- advent/day2/go.mod
+++ advent/day2/go.mod
@@ -1,3 +1,5 @@
 module day2
 
 go 1.26.3
+
+require github.com/dlclark/regexp2 v1.12.0
--- advent/day2/main.go
+++ advent/day2/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"os"
 	"fmt"
 	"log"
-	"strings"
+	"os"
 	"strconv"
+	"strings"
+
+	"github.com/dlclark/regexp2"
 )
 
 func is_even(value string) bool {
-	if len(value) % 2 == 0 {
+	if len(value)%2 == 0 {
 		return true
 	} else {
 		return false
@@ -23,31 +25,43 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	productIds := strings.Split(string(data), ",")
 
-	for _, idRange := range productIds {
+	for idRange := range strings.SplitSeq(string(data), ",") {
 		idRange = strings.TrimSpace(idRange)
 		if idRange == "" {
 			continue
 		}
 		split := strings.Split(idRange, "-")
-		first_bound,err1 := strconv.Atoi(split[0])
+		first_bound, err1 := strconv.Atoi(split[0])
 		last_bound, err2 := strconv.Atoi(split[1])
-		
+
 		if err1 != nil || err2 != nil {
 			fmt.Printf("Error1: %s\n Error2: %s", err1, err2)
 			log.Fatal("Failed to parse bounds")
 		}
-		for i:=first_bound; i<=last_bound; i++ {
-			idString := strconv.Itoa(i)	
+		for i := first_bound; i <= last_bound; i++ {
+			idString := strconv.Itoa(i)
 
-			if len(idString) % 2 == 0 {
-				half := len(idString) / 2
-				if idString[:half] == idString[half:] {
-					sum += i
-				}
+			pattern := regexp2.MustCompile(`^(\d+)\1+$`, 0)
+
+			m, err := pattern.FindStringMatch(idString)
+			if err != nil {
+				log.Fatal(err)
 			}
+
+			if m != nil {
+				sum += i
+			}
+			// PART 1
+			// if m {
+			// 	sum += i
+			// }
+			// if len(idString)%2 == 0 {
+			// 	half := len(idString) / 2
+			// 	if idString[:half] == idString[half:] {
+			// 		sum += i
+			// 	}
+			// }
 		}
 	}
 	fmt.Printf("Sum: %d", sum)
