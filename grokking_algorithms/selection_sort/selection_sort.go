96cca75be25b002f9b84b354682ddf087ea343ed
Jordan Kaiser <jordan.kaiser@gmail.com>
2026-07-15 15:40:54 -0400

    selection sort stuff

--- grokking_algorithms/selection_sort/selection_sort.go
+++ grokking_algorithms/selection_sort/selection_sort.go
@@ -1,9 +1,23 @@
 package main
-
 import (
 	"fmt"
 )
 
+
+func findSmallest(arr []int) int{
+	smallest := arr[0]
+	smallest_index := 0
+	
+	for i := range arr {
+		if arr[i] < smallest {
+			smallest = arr[i]
+			smallest_index = i
+		}
+	}
+	return smallest_index
+}
+
 func main () {
- fmt.Print("Start")
+	start_index := findSmallest([]int{4,2,9,22})
+	fmt.Printf("Smallest index is: %d\n", start_index)
 }
