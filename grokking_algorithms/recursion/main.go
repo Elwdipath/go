1dba82b7d82ff0b3981ede17e828e605c136e649
Jordan Kaiser <jordan.kaiser@gmail.com>
2026-07-22 21:18:18 -0400

    recursion

---
+++ grokking_algorithms/recursion/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func countdown(i int){
+	fmt.Printf("%d\n", i)
+
+	if i <= 1 {
+		return
+	} else {
+		time.Sleep(1 * time.Second)
+		countdown(i - 1)
+	}
+}
+
+func main() {
+	countdown(3)
+}
